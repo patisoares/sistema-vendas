626153c5ae86cdfe91d226909c125bb1714b06ba
patisoares <pati_soares75@hotmail.com>
2020-12-01 22:01:22 -0300

    Refatoração

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const {db} = require('./src/db/connection')
+const clienteController = require('./src/controllers/ClienteController')
 
-/*db.query('SELECT * FROM cliente', (err.result)=>{
-  if (err){
-    console.log(`Houve um erro ao conectar: ${err}`)
-  }
-  console.log(result.rows)
-});*/
+
+
 
 const app = express()
 const port = 3000
@@ -35,30 +31,12 @@ app.get('/categoria-produto/adicionar',(req,res)=>{
 
 //ROTAS PARA CADASTRO DE CLIENTES
 
-app.get('/cliente/listar',(req,res)=>{
-  db.query('SELECT * FROM cliente', (err,result)=>{
-    if(err){
-      console.log(`Houve um erro ao listar os clientes: ${err}`)
-    }
-    res.render('cliente/listar',{clientes:result.rows})
-  })
-})
-
-app.get('/cliente/adicionar',(req,res)=>{
-  res.render('cliente/adicionar')
-})
-
-app.post('/cliente/salvar',(req,res)=>{
-  const query = {
-  text: 'INSERT INTO cliente(nome,cpf) VALUES ($1,$2)',
-  values:[req.body.nome,req.body.cpf]
-  }
-  db.query(query,(err,result)=>{
-    console.log(result)
-  })
-  res.redirect('/cliente/listar')
-})
-
+app.get('/cliente/listar',clienteController.index)
+app.get('/cliente/adicionar',clienteController.create)
+app.post('/cliente/salvar',clienteController.store)
+app.get('/cliente/editar/:id',clienteController.edit)
+app.post('/cliente/atualizar', clienteController.update)
+app.get('/cliente/excluir/:id', clienteController.delete)
 //rotas para forma de pagamento
 app.get('/forma-pagamento/listar',(req,res)=>{
   db.query('SELECT * FROM forma_pagamento',(err,result)=>{
